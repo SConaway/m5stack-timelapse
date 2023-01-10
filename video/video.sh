1b320cf803eeee17aa1b0cf49f39fc7aa0be2880
Steven Conaway <steven.conaway@icloud.com>
2023-01-09 18:18:31 -0700

    small changes for UX

--- video/video.sh
+++ video/video.sh
@@ -3,11 +3,14 @@
 # set failure
 set -e
 
+# cd to the directory of this script
+cd "$(dirname "$0")"
+
 # call timestamp.js
 node timestamp.js
 
 # call ffmpeg script
 ./ffmpeg.sh
 
-# print video file size
-ls -l ../images/video.mp4 | awk '{print $5}'
+# print video file size in MB
+ls -l ../images/video.mp4 | awk '{print $5/1000000}' | xargs -I {} echo "Video file size: {} MB"
